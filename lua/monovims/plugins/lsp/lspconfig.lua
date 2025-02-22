0849c31f128325bd63adfcd8638f1f329a2a703f
raferianluba <ndrafebut@gmail.com>
2025-02-22 18:25:48 +0800

    odin lang precedent

--- lua/monovims/plugins/lsp/lspconfig.lua
+++ lua/monovims/plugins/lsp/lspconfig.lua
@@ -70,6 +70,11 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
+    lspconfig["ols"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     -- configure html server
     lspconfig["html"].setup({
       capabilities = capabilities,
