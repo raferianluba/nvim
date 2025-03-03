a5ad8782230422fae87921b8eed53820eb285d79
raferianluba <ndrafebut@gmail.com>
2025-03-03 21:36:37 +0800

    sum stuff

--- lua/monovims/plugins/lsp/lspconfig.lua
+++ lua/monovims/plugins/lsp/lspconfig.lua
@@ -177,6 +177,10 @@ return {
     --}, 
     })
 
+    lspconfig["eslint_d"].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+    })
     -- configure gleam !!!
     lspconfig["gleam"].setup({
       capabilities = capabilities,
